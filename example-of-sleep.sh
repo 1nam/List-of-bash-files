0e9a131db35e58096563f313cfd4da69a0fd2d53
zeus <33592162+1nam@users.noreply.github.com>
2020-10-10 21:40:27 -0400

    Create example-of-sleep.sh

---
+++ example-of-sleep.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#1nam testing sleep
+
+echo "How are you doing?: "
+read user_input
+sleep 3
+echo "thinking..."
+sleep 2
+echo "thinking......"
+sleep 1
+echo "thinking........."
+echo "Ahh ok! you said. $user_input"
