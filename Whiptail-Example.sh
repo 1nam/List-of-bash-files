4d1f20386f71e621d6d4379e1f05297aadb7a2c8
zeus <33592162+1nam@users.noreply.github.com>
2020-10-14 12:47:17 -0400

    Create Whiptail-Example.sh

---
+++ Whiptail-Example.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#1nam   testing Whiptail --gauge
+# enter any name and pass and a log file is created.
+# log file will be in the same directory as this script.
+
+clear
+read -p "user-name: " input1
+    read -p "user-pass: " input2
+    read -p "To Login press enter"
+{
+    for ((i = 0 ; i <= 100 ; i+=5)); do
+        sleep 1  # Default 0.1
+        echo $i
+    done
+} | whiptail --gauge "Logging in..." 6 50 0
+echo $(date +"%m-%d-%Y") "- user-name: $input1 pass: $input2" > log_of_Login.txt
+echo "A Log File Has Been Created In The Same Directory As This Script."
+sleep 5
+clear
+exit
