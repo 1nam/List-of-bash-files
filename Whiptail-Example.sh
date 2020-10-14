2a75388b0955f1f4c27e17c49cea6b8d58629792
zeus <33592162+1nam@users.noreply.github.com>
2020-10-14 12:48:17 -0400

    Update Whiptail-Example.sh

--- Whiptail-Example.sh
+++ Whiptail-Example.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #1nam   testing Whiptail --gauge
-# enter any name and pass and a log file is created.
+# enter any name and pass and a log file is created with current date.
 # log file will be in the same directory as this script.
 
 clear
