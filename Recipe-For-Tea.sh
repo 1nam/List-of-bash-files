15d38df1a052039aa38ad2da7d7f2b0227c9040c
zeus <33592162+1nam@users.noreply.github.com>
2020-10-14 16:46:29 -0400

    Create Recipe-For-Tea.sh

---
+++ Recipe-For-Tea.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#1nam  Testing Loop&break:-> my bash script for a Recipe-For-Tea.sh
+
+while true
+do
+clear
+echo ""
+echo "Make the perfect cup of Tea"
+echo ""
+sleep 5
+echo ""
+echo "Never Boil The Water you steep it. Tea is not ment to be Boiled."
+echo ""
+sleep 5
+echo ""
+echo "One 8 Ounce Glass"
+echo ""
+sleep 5
+echo ""
+echo "wett the rim of the glass with water. Dip the rim of the glass in sugar."
+echo ""
+sleep 5
+echo ""
+echo "Add 2 and 1/2 table spoons of sugar. pour steeped water in to Glass Mix"
+echo ""
+sleep 5
+echo ""
+echo "fill Glass with crushed ice, and top with a lemon slice."
+echo ""
+sleep 5
+clear
+break
+done
+exit
