0a82195aec5631d0716165696269faa4cc174edb
z7eus <33592162+1nam@users.noreply.github.com>
2025-09-09 17:18:03 -0400

    Create New_peek_workspace.sh

---
+++ New_peek_workspace.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# No unnecessary clear.
+
+# Peek moves forward then back, instead of forward twice.
+
+# ingle read -p makes interaction smoother.
+
+Clean and readable.
+while true; do
+    read -p "Peek at another workspace for 10 seconds (press x to peek, any other key to quit): " UI
+
+    if [[ $UI == "x" ]]; then
+        # Move to next workspace
+        xdotool set_desktop --relative 1
+        sleep 10  # peek duration
+        # Return to original workspace
+        xdotool set_desktop --relative -1
+    else
+        echo "Goodbye..."
+        sleep 2
+        break
+    fi
+done
