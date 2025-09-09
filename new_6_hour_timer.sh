aa64949dc097f17e91366c8ee91ae6b11445abd7
z7eus <33592162+1nam@users.noreply.github.com>
2025-09-09 17:07:05 -0400

    Create new_6_hour_timer.sh

---
+++ new_6_hour_timer.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+seconds=21600  # 6 hours
+
+while [ $seconds -ge 0 ]; do
+    # Convert seconds to HH:MM:SS
+    h=$((seconds/3600))
+    m=$((seconds%3600/60))
+    s=$((seconds%60))
+    printf "\r⏳ Timer: %02d:%02d:%02d remaining  " $h $m $s
+
+    sleep 1
+    ((seconds--))
+done
+
+echo -e "\n✅ Finished!"
